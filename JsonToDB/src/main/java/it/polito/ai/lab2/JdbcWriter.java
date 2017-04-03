4f236c412454859084fa48d2e5781f03342d2876
Martino Mensio <martinomensio@outlook.it>
2017-04-03 15:57:27 +0200

    jdbc connection

--- JsonToDB/src/main/java/it/polito/ai/lab2/JdbcWriter.java
+++ JsonToDB/src/main/java/it/polito/ai/lab2/JdbcWriter.java
@@ -1,5 +1,38 @@
 package it.polito.ai.lab2;
 
-public class JdbcWriter {
+import java.sql.*;
 
+public class JdbcWriter {
+	
+	private Connection connection;
+
+	public JdbcWriter() {
+		try {
+			Class.forName("org.postgresql.Driver");
+
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/trasporti", "postgres",
+					"ai-user-password");
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public boolean insertBusLine(Object busline) {
+		// TODO
+		return false;
+	}
+	
+	public boolean insertBusStop(Object busstop) {
+		// TODO
+		return false;
+	}
+
+	public void close() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- JsonToDB/src/main/java/it/polito/ai/lab2/Main.java
+++ JsonToDB/src/main/java/it/polito/ai/lab2/Main.java
@@ -3,8 +3,16 @@ package it.polito.ai.lab2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		JdbcWriter jdbcWriter = new JdbcWriter();
+		System.out.println("Connected to db");
+		
+		// TODO ask to JsonFileReader all the data
+		
+		// TODO put all the data in DB calling JdbcWriter
+		
+		jdbcWriter.close();
+		System.out.println("done");
 	}
 
 }
