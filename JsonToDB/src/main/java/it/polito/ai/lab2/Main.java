08b183a06772e595cfad571af95e59961d9cd6d7
Riccardo Persiani <s225289@studenti.polito.it>
2017-04-05 14:05:37 +0200

    Access in static way

--- JsonToDB/src/main/java/it/polito/ai/lab2/Main.java
+++ JsonToDB/src/main/java/it/polito/ai/lab2/Main.java
@@ -1,6 +1,5 @@
 package it.polito.ai.lab2;
 
-import java.util.List;
 import java.util.Set;
 
 import it.polito.ai.lab2.objects.*;
@@ -10,15 +9,15 @@ public class Main {
 	public static void main(String[] args) {
 		
 		JdbcWriter jdbcWriter = new JdbcWriter();
-		JsonFileReader jfr = new JsonFileReader();
+		//JsonFileReader jfr = new JsonFileReader();
 		System.out.println("Connected to db");
 		
 		// parsing Json file to Java objects
-		jfr.readJson();
+		JsonFileReader.readJson();
 		
 		// TODO ask to JsonFileReader all the data
-		Set<BusLine> busLines = jfr.busLines;
-		Set<BusStop> busStops = jfr.busStops;
+		Set<BusLine> busLines = JsonFileReader.busLines;
+		Set<BusStop> busStops = JsonFileReader.busStops;
 		
 		// put all the data in DB calling JdbcWriter
 		for (BusStop busStop : busStops) {
