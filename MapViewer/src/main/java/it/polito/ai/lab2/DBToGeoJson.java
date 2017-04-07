9243722272a7a9d3c7a7f41dc94dceb393a20655
Riccardo Persiani <s225289@studenti.polito.it>
2017-04-07 15:40:01 +0200

    retreiving busStops with a query

--- MapViewer/src/main/java/it/polito/ai/lab2/DBToGeoJson.java
+++ MapViewer/src/main/java/it/polito/ai/lab2/DBToGeoJson.java
@@ -1,23 +1,57 @@
 package it.polito.ai.lab2;
 
 import java.io.IOException;
+import java.util.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import it.polito.ai.lab2.entities.BusLine;
+import it.polito.ai.lab2.entities.BusStop;
+
 public class DBToGeoJson extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public DBToGeoJson() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
+	public DBToGeoJson() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	// Preleva i dati dal database e li traduce in un file .json seguendo il modello GeoJSON
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// Salvataggio in una lista di BusLine e di BusStop dei dati presenti nel database
+		request.getSession(); //TODO Sostituire?
+		String line = request.getParameter("line");
+
+		Query query = ((Session) request.getAttribute("session")).createQuery("from busline where busline.line=:line");
+		query.setString("line", line);
+		BusLine busLine = (BusLine) query.list();
+		
+		List<BusStop> busStopList = busLine.getStops();
+
+		// Parsing delle linee
+		for(BusLine line : busLineList){
+			busLineToGeoJson(line);
+		}
+		// Parsing degli stop
+		for(BusStop stop : busStopList){
+			busStopToGeoJson(stop);
+		}
+
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected JSONObject busLineToGeoJson(BusLine busLine){
+		// Traduzione in Json
 		JSONObject featureCollection = new JSONObject();
 		featureCollection.put("type", "FeatureCollection");
 		JSONObject properties = new JSONObject();
@@ -31,7 +65,14 @@ public class DBToGeoJson extends HttpServlet {
 		JSONObject feature = new JSONObject();
 		feature.put("type", "Feature");
 		JSONObject geometry = new JSONObject();
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		return null;
+
+	}
+
+	protected JSONObject busStopToGeoJson(BusStop busStop){
+		return null;
+
 	}
 
 
